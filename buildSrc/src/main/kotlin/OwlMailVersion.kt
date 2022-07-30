b2ca475323c2d92aa0e458e4fe1cd146b3098ebf
preeti5sharon <85344743+preeti5sharon@users.noreply.github.com>
2022-07-30 19:24:26 +0530

    moved versions to constants

--- buildSrc/src/main/kotlin/OwlMailVersion.kt
+++ buildSrc/src/main/kotlin/OwlMailVersion.kt
@@ -1,4 +1,18 @@
+import org.gradle.api.JavaVersion
+
 object OwlMailVersion {
+    const val CompileSdk = 32
+    const val MinSdk = 23
+    const val TargetSdk = 32
+
+    val JDKVersion = JavaVersion.VERSION_1_8
+    val JvmTarget = JDKVersion.toString()
+
+    const val ApplicationId = "github.owlmail.app"
+    const val AppVersionCode = 1
+    const val AppVersionName = "1.0"
+
+
 
     const val CoreKTX = "1.8.0"
     const val AppCompat = "1.4.2"
