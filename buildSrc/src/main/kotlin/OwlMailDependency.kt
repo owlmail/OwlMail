f204d7d97cf6c091d54f005d6eb051d9cdb5082b
preeti5sharon <85344743+preeti5sharon@users.noreply.github.com>
2022-07-30 19:32:03 +0530

    Moved dependencies to constants (#5)
    * initial commit
    
    * moved dependency to constants
    
    * moved versions to constants

---
+++ buildSrc/src/main/kotlin/OwlMailDependency.kt
@@ -0,0 +1,11 @@
+object OwlMailDependency {
+
+    const val CoreKTX = "androidx.core:core-ktx:${OwlMailVersion.CoreKTX}"
+    const val AppCompat = "androidx.appcompat:appcompat:${OwlMailVersion.AppCompat}"
+    const val Material = "com.google.android.material:material:${OwlMailVersion.Material}"
+    const val ConstraintLayout =
+        "androidx.constraintlayout:constraintlayout:${OwlMailVersion.ConstraintLayout}"
+    const val Junit = "junit:junit:${OwlMailVersion.Junit}"
+    const val JunitAndroid = "androidx.test.ext:junit:${OwlMailVersion.JunitAndroid}"
+    const val EspressoCore = "androidx.test.espresso:espresso-core:${OwlMailVersion.Espresso}"
+}
---
+++ buildSrc/src/main/kotlin/OwlMailVersion.kt
@@ -0,0 +1,24 @@
+import org.gradle.api.JavaVersion
+
+object OwlMailVersion {
+    const val CompileSdk = 32
+    const val MinSdk = 23
+    const val TargetSdk = 32
+
+    val JDKVersion = JavaVersion.VERSION_1_8
+    val JvmTarget = JDKVersion.toString()
+
+    const val ApplicationId = "github.owlmail.app"
+    const val AppVersionCode = 1
+    const val AppVersionName = "1.0"
+
+
+
+    const val CoreKTX = "1.8.0"
+    const val AppCompat = "1.4.2"
+    const val Material = "1.6.1"
+    const val ConstraintLayout = "2.1.4"
+    const val Junit = "4.13.2"
+    const val JunitAndroid = "1.1.3"
+    const val Espresso = "3.4.0"
+}
