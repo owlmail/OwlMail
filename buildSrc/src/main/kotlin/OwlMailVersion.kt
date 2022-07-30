39fdbde2186b95d820771d1d9fade7ab754fcfff
preeti5sharon <85344743+preeti5sharon@users.noreply.github.com>
2022-07-30 19:02:26 +0530

    moved dependency to constants

---
+++ buildSrc/src/main/kotlin/OwlMailVersion.kt
@@ -0,0 +1,10 @@
+object OwlMailVersion {
+
+    const val CoreKTX = "1.8.0"
+    const val AppCompat = "1.4.2"
+    const val Material = "1.6.1"
+    const val ConstraintLayout = "2.1.4"
+    const val Junit = "4.13.2"
+    const val JunitAndroid = "1.1.3"
+    const val Espresso = "3.4.0"
+}
