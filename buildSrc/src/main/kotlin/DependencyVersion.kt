94125123eea94e492f2176ec5f1af1df16e842a1
Sachin Dehury <51115041+sachin2dehury@users.noreply.github.com>
2022-07-31 22:34:10 +0530

    Organised Constants (#8)

---
+++ buildSrc/src/main/kotlin/DependencyVersion.kt
@@ -0,0 +1,16 @@
+object DependencyVersion {
+
+
+    const val CoreKTX = "1.8.0"
+    const val AppCompat = "1.4.2"
+
+    const val Material = "1.6.1"
+    const val ConstraintLayout = "2.1.4"
+
+    const val Junit = "4.13.2"
+    const val JunitAndroid = "1.1.3"
+    const val Espresso = "3.4.0"
+
+
+    const val DaggerHilt = "2.43"
+}
