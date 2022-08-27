98e6ddc53845ba3efd87e82c4aabcf1ae0cb14c7
Sachin Dehury <51115041+sachin2dehury@users.noreply.github.com>
2022-08-27 12:36:20 +0530

    Added Firebase

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,20 +1,29 @@
 object Dependencies {
 
-
+    // Core
     const val CoreKTX = "androidx.core:core-ktx:${DependencyVersion.CoreKTX}"
     const val AppCompat = "androidx.appcompat:appcompat:${DependencyVersion.AppCompat}"
 
+    // Ui
     const val Material = "com.google.android.material:material:${DependencyVersion.Material}"
     const val ConstraintLayout =
         "androidx.constraintlayout:constraintlayout:${DependencyVersion.ConstraintLayout}"
 
-    const val Junit = "junit:junit:${DependencyVersion.Junit}"
-    const val JunitAndroid = "androidx.test.ext:junit:${DependencyVersion.JunitAndroid}"
-    const val EspressoCore = "androidx.test.espresso:espresso-core:${DependencyVersion.Espresso}"
-
-
+    // Hilt
     const val DaggerHilt = "com.google.dagger:hilt-android:${DependencyVersion.DaggerHilt}"
     const val DaggerHiltCompiler =
         "com.google.dagger:hilt-android-compiler:${DependencyVersion.DaggerHilt}"
-    const val DaggerHiltClassPath = "com.google.dagger:hilt-android-gradle-plugin:${DependencyVersion.DaggerHilt}"
+
+
+    // Firebase
+    const val FirebaseBom = "com.google.firebase:firebase-bom:${DependencyVersion.FirebaseBom}"
+    const val FirebasePerformance = "com.google.firebase:firebase-perf-ktx"
+    const val FirebaseRemoteConfig = "com.google.firebase:firebase-config-ktx"
+    const val FirebaseAnalytics = "com.google.firebase:firebase-analytics-ktx"
+    const val FirebaseCrashlytics = "com.google.firebase:firebase-crashlytics-ktx"
+
+    // Test
+    const val Junit = "junit:junit:${DependencyVersion.Junit}"
+    const val JunitAndroid = "androidx.test.ext:junit:${DependencyVersion.JunitAndroid}"
+    const val EspressoCore = "androidx.test.espresso:espresso-core:${DependencyVersion.Espresso}"
 }
--- buildSrc/src/main/kotlin/Modules.kt
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -1,2 +1,9 @@
 object Modules {
+
+    const val Auth = ":auth"
+    const val Core = ":core"
+    const val Design = ":design"
+    const val Mail = ":mail"
+    const val LintRules = ":lint-rules"
+    const val Networking = ":networking"
 }
