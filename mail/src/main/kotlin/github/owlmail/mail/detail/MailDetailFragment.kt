89bc51f1ff85e6a2809dc3712b1e858bb3050dd7
preeti5sharon <85344743+preeti5sharon@users.noreply.github.com>
2023-01-16 21:58:02 +0530

    added dummy detail screen

---
+++ mail/src/main/kotlin/github/owlmail/mail/detail/MailDetailFragment.kt
@@ -0,0 +1,11 @@
+package github.owlmail.mail.detail
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import github.owlmail.mail.databinding.MailDetailsBinding
+
+@AndroidEntryPoint
+class MailDetailFragment : Fragment() {
+    private var binding : MailDetailsBinding? = null
+    
+}
---
+++ mail/src/main/kotlin/github/owlmail/mail/detail/MailDetailResponse.kt
@@ -0,0 +1,121 @@
+package github.owlmail.mail.detail
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class MailDetailResponse(
+    @Json(name = "Body")
+    val body: Body? = null,
+    @Json(name = "Header")
+    val header: Header? = null,
+    @Json(name = "_jsns")
+    val jsns: String? = null
+) {
+    @JsonClass(generateAdapter = true)
+    data class Body(
+        @Json(name = "SearchConvResponse")
+        val searchConvResponse: SearchConvResponse? = null
+    ) {
+        @JsonClass(generateAdapter = true)
+        data class SearchConvResponse(
+            @Json(name = "_jsns")
+            val jsns: String? = null,
+            @Json(name = "m")
+            val m: List<M?>? = null,
+            @Json(name = "more")
+            val more: Boolean? = null,
+            @Json(name = "offset")
+            val offset: String? = null,
+            @Json(name = "sortBy")
+            val sortBy: String? = null
+        ) {
+            @JsonClass(generateAdapter = true)
+            data class M(
+                @Json(name = "cid")
+                val cid: String? = null,
+                @Json(name = "cm")
+                val cm: Boolean? = null,
+                @Json(name = "d")
+                val d: Long? = null,
+                @Json(name = "e")
+                val e: List<E?>? = null,
+                @Json(name = "fr")
+                val fr: String? = null,
+                @Json(name = "id")
+                val id: String? = null,
+                @Json(name = "l")
+                val l: String? = null,
+                @Json(name = "mid")
+                val mid: String? = null,
+                @Json(name = "mp")
+                val mp: List<Mp?>? = null,
+                @Json(name = "rev")
+                val rev: Int? = null,
+                @Json(name = "s")
+                val s: Int? = null,
+                @Json(name = "sd")
+                val sd: Long? = null,
+                @Json(name = "sf")
+                val sf: String? = null,
+                @Json(name = "su")
+                val su: String? = null
+            ) {
+                @JsonClass(generateAdapter = true)
+                data class E(
+                    @Json(name = "a")
+                    val a: String? = null,
+                    @Json(name = "d")
+                    val d: String? = null,
+                    @Json(name = "t")
+                    val t: String? = null
+                )
+
+                @JsonClass(generateAdapter = true)
+                data class Mp(
+                    @Json(name = "body")
+                    val body: Boolean? = null,
+                    @Json(name = "content")
+                    val content: String? = null,
+                    @Json(name = "ct")
+                    val ct: String? = null,
+                    @Json(name = "part")
+                    val part: String? = null,
+                    @Json(name = "s")
+                    val s: Int? = null
+                )
+            }
+        }
+    }
+
+    @JsonClass(generateAdapter = true)
+    data class Header(
+        @Json(name = "context")
+        val context: Context? = null
+    ) {
+        @JsonClass(generateAdapter = true)
+        data class Context(
+            @Json(name = "change")
+            val change: Change? = null,
+            @Json(name = "_jsns")
+            val jsns: String? = null,
+            @Json(name = "session")
+            val session: Session? = null
+        ) {
+            @JsonClass(generateAdapter = true)
+            data class Change(
+                @Json(name = "token")
+                val token: Int? = null
+            )
+
+            @JsonClass(generateAdapter = true)
+            data class Session(
+                @Json(name = "_content")
+                val content: String? = null,
+                @Json(name = "id")
+                val id: String? = null
+            )
+        }
+    }
+}
