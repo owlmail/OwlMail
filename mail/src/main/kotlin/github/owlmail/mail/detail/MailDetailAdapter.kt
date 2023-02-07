b36dd5722add2701b52084c6de1fb866b1885618
preeti5sharon <85344743+preeti5sharon@users.noreply.github.com>
2023-02-07 12:40:48 +0530

    added dynamic attachment

--- mail/src/main/kotlin/github/owlmail/mail/detail/MailDetailAdapter.kt
+++ mail/src/main/kotlin/github/owlmail/mail/detail/MailDetailAdapter.kt
@@ -3,6 +3,7 @@ package github.owlmail.mail.detail
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -11,6 +12,7 @@ import github.owlmail.mail.databinding.ConvMailBodyBinding
 import github.owlmail.mail.detail.model.MailDetailResponse
 
 class MailDetailAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    var onClick: ((String?,String?,String?) -> Unit)? = null
     private val diffUtilCallback =
         object : DiffUtil.ItemCallback<MailDetailResponse.Body.SearchConvResponse.Message>() {
             override fun areItemsTheSame(
@@ -61,6 +63,9 @@ class MailDetailAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
             html
         }
         binding.mailBody.setHtml(messageBody)
+        item?.multiPart?.map {
+            it?.renderAttachmentButton(binding,item.id)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -76,6 +81,28 @@ class MailDetailAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
             contentDescription?.equals("attachment", true) == true -> "" //to download the file
             //https://mail.nitrkl.ac.in/service/home/~/?auth=co&loc=en_GB&id=19923&part=2
             else -> ""
+        }
+    }
+
+    private fun MailDetailResponse.Body.SearchConvResponse.Message.MultiPart.renderAttachmentButton(
+        binding: ConvMailBodyBinding,
+        messageId: String?
+    ) {
+        when {
+            contentType?.contains("multipart", true) == true -> multiPart?.map{
+                it?.renderAttachmentButton(binding,messageId)
+            }
+            contentDescription?.equals("attachment", true) == true -> {
+                val button = Button(binding.root.context).apply {
+                    text = fileName
+                    setOnClickListener{
+                        onClick?.invoke(fileName,part,messageId)
+                    }
+                }
+                binding.attachmentContainer.addView(button)
+            }//to download the file
+            //https://mail.nitrkl.ac.in/service/home/~/?auth=co&loc=en_GB&id=19923&part=2
+            else -> {}
         }
     }
 }
