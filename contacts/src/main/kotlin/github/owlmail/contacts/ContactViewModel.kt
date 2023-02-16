c336594efc7865527c941f4a4660d6eba8d84164
preeti5sharon <85344743+preeti5sharon@users.noreply.github.com>
2023-02-16 19:44:43 +0530

    removed livedata

--- contacts/src/main/kotlin/github/owlmail/contacts/ContactViewModel.kt
+++ contacts/src/main/kotlin/github/owlmail/contacts/ContactViewModel.kt
@@ -8,6 +8,8 @@ import androidx.paging.PagingData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import github.owlmail.contacts.model.ContactResponse
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,11 +17,17 @@ class ContactViewModel @Inject constructor(
     private val repository: ContactRepository,
     private val contactDAO: ContactDAO
 ) : ViewModel() {
+    private val searchQuery = MutableStateFlow("")
     private val pagingConfig = PagingConfig(pageSize = 10, 10, false, 10)
-    fun getPaginatedData(searchContact: String, context: Context): Flow<PagingData<ContactResponse.Body.SearchGalResponse.Cn>> {
-        return Pager(pagingConfig, 0) {
+    fun getPaginatedData(context: Context): Flow<PagingData<ContactResponse.Body.SearchGalResponse.Cn>> {
+        return searchQuery.flatMapLatest {
+            Pager(pagingConfig, 0) {
 
-            ContactPagingSource(repository, searchContact, contactDAO, context)
-        }.flow
+                ContactPagingSource(repository, it, contactDAO, context)
+            }.flow
+        }
+    }
+    fun updateSearchQuery(query: String) {
+        searchQuery.value = query
     }
 }
